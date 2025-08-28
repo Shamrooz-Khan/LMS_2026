5fe4c0a0c495201e3b2583ee4193d11ed2935a85
osman-dev01 <usmanam004@gmail.com>
2025-08-28 12:34:48 +0100

    removed venv

---
+++ core/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2', 'role']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])  # ensure password is hashed
+        if commit:
+            user.save()
+        return user
